1f0488f34c76b6b71ed0ff7e4e138f56fc834f37
prcordova <43557558+prcordova@users.noreply.github.com>
2019-05-20 11:26:23 -0300

    Update

---
+++ fundamentos.js/Strings.js
@@ -0,0 +1,16 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3)) //51 é o código relacionado a tabela ask ( unicode)
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split('.'))
+
+console.log('Escola ' + escola + "!")
--- fundamentos.js/variaveisEConstantes.js
+++ fundamentos.js/variaveisEConstantes.js
@@ -5,3 +5,13 @@ var a = 30
 b = 40
 
 console.log(a, b)
+
+a = 300
+b = 400 // variaveis após definidas, podem ser alteradas quantas vezes necessarias.
+
+console.log(a, b)
+
+const c = 5  // nao permite novos resultados para 1 variavel
+//c = 50
+console.log(c)
+
