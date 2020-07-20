2650442fa71dbe1746ca27e865911e2c0044d863
prcordova <43557558+prcordova@users.noreply.github.com>
2020-07-20 05:50:04 -0300

    aula90 concluida (ObjetoRevisao)

--- objeto/oo.js
+++ objeto/oo.js
@@ -1,1 +1,22 @@
 //codigo não executavel!!!
+
+//procedural 
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // foco passou a ser OO
+
+//principios importantes:
+//1. abstracao
+//2. encapsulamento
+//3. heranca
+//4. polimorfismo (prototype)
