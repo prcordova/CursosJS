dfbe489c6c64442bae819477a5e0fe33a1cc3ed1
Pedro Ricardo Oliveira Córdova <43557558+prcordova@users.noreply.github.com>
2021-01-19 01:11:48 -0300

    aula 103 completa

---
+++ objeto/heranca2.js
@@ -0,0 +1,48 @@
+//cadeia de prototipos (prototype chain)
+Object.prototype.attr0 = '0' // nao fazer esse tipo de manipulação, para nao impactar na estrutura global.
+
+const avo = { attr1: 'A' }
+const pai = { __proto__: avo, attr2: 'B', attr3: 'C de pai'}
+const filho = { __proto__: pai, attr3: 'C' }
+
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelerarMais(delta) {
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        } else {
+            this.velAtual = this.velMax
+        }
+    },
+    status() {
+        return `${this.velAtual}km/h de ${this.velMax}km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324,
+
+}
+
+const volvo = {
+    modelo: 'V40',
+    status() {
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+
+volvo.acelerarMais(100)
+console.log(volvo.status())
+
+ferrari.acelerarMais(323)
+console.log(ferrari.status())
